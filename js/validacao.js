e731f10e23d8a7b35d1f79119dd30b443d62726d
MasterKelwin <mkelwin@outlook.com>
2022-03-17 08:21:17 -0300

    validação CPF

--- js/validacao.js
+++ js/validacao.js
@@ -2,21 +2,62 @@ export function valida(input) {
     const tipoDeInput = input.dataset.tipo 
 
     if(validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input)
+        validadores[tipoDeInput](input);
     } 
     
     if(input.validity.valid) {
-        input.parentElement.classList.remove('input-container--invalido')
-        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
     } else {
-        input.parentElement.classList.add('input-container--invalido')
-        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
     }
 }
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input)
 }
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio'
+    },
+
+    email: {
+        valueMissing: 'O campo email não pode estar vazio',
+        typeMismatch: 'O email não é válido'
+    },
+
+    senha: {
+        valueMissing: 'O campo senha não pode estar vazio',
+        patternMismatch: 'A senha deve conter entre 8 e 24 caracteres, deve conter pelo menos uma letra minúscula, uma maiúscula e um número'
+    },
+
+    dataNascimento: {
+        valueMissing: 'O campo data de nascimento não pode estar vazio',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar'
+    },
+
+    cpf: {
+        valueMissing: 'O campo CPF não pode estar vazio',
+        customError: 'O CPF digitado não é válido'
+    }
+}
+
+const tiposDeErro = [
+    'badInput',
+    'customError',
+    'patternMismatch',
+    'rangeOverflow',
+    'rangeUnderflow',
+    'stepMismatch',
+    'tooLong',
+    'tooShort',
+    'typeMismatch',
+    'valid',
+    'valueMissing'
+]
 
 function mostraMensagemDeErro(tipoDeInput, input) {
     let mensagem = ''
@@ -48,39 +89,37 @@ function maiorQue18(data) {
     return dataMais18 <= dataAtual;
 }
 
+function validaCPF(input) {
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
 
-
-const mensagensDeErro = {
-    nome: {
-        valueMissing: 'O campo nome não pode estar vazio'
-    },
-
-    email: {
-        valueMissing: 'O campo email não pode estar vazio',
-        typeMismatch: 'O email não é válido'
-    },
-
-    senha: {
-        valueMissing: 'O campo senha não pode estar vazio',
-        patternMismatch: 'A senha deve conter entre 8 e 24 caracteres, deve conter pelo menos uma letra minúscula, uma maiúscula e um número'
-    },
-
-    dataNascimento: {
-        valueMissing: 'O campo data de nascimento não pode estar vazio',
-        customError: 'Você deve ser maior que 18 anos para se cadastrar'
+    if(!checaCPFRepetido(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é válido'
     }
+
+    input.setCustomValidity(mensagem);
 }
 
-const tiposDeErro = [
-    'badInput',
-    'customError',
-    'patternMismatch',
-    'rangeOverflow',
-    'rangeUnderflow',
-    'stepMismatch',
-    'tooLong',
-    'tooShort',
-    'typeMismatch',
-    'valid',
-    'valueMissing'
-]
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999',
+        '00000000000'
+    ]
+
+    let cpfValido = true;
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf) {
+            cpfValido = false
+        }
+    }) 
+
+    return cpfValido;
+}
