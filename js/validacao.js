cf893020ac64be55662dd683bd61404e8e5f318c
MasterKelwin <mkelwin@outlook.com>
2022-03-17 09:39:25 -0300

    validação completa de dados pessoais

--- js/validacao.js
+++ js/validacao.js
@@ -2,15 +2,15 @@ export function valida(input) {
     const tipoDeInput = input.dataset.tipo 
 
     if(validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input);
+        validadores[tipoDeInput](input)
     } 
     
     if(input.validity.valid) {
-        input.parentElement.classList.remove('input-container--invalido');
-        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
     } else {
-        input.parentElement.classList.add('input-container--invalido');
-        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
     }
 }
 
@@ -68,7 +68,7 @@ function mostraMensagemDeErro(tipoDeInput, input) {
         }
     })
 
-    return mensagem;
+    return mensagem
 }
 
 function validaDataNascimento(input) {
@@ -84,20 +84,20 @@ function validaDataNascimento(input) {
 
 function maiorQue18(data) {
     const dataAtual = new Date();  //date() é automaticamente preenchido com a data atual
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
-    return dataMais18 <= dataAtual;
+    return dataMais18 <= dataAtual
 }
 
 function validaCPF(input) {
     const cpfFormatado = input.value.replace(/\D/g, '')
     let mensagem = ''
 
-    if(!checaCPFRepetido(cpfFormatado)) {
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstrutraCPF(cpfFormatado)) {
         mensagem = 'O CPF digitado não é válido'
     }
 
-    input.setCustomValidity(mensagem);
+    input.setCustomValidity(mensagem)
 }
 
 function checaCPFRepetido(cpf) {
@@ -121,15 +121,36 @@ function checaCPFRepetido(cpf) {
         }
     }) 
 
-    return cpfValido;
+    return cpfValido
 }
 
 function checaEstrutraCPF(cpf) {
-    const multiplicador = 10;
+    const multiplicador = 10
     
-    return checaDigitoVerificador(cpf, multiplicador);
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function checaDigitoVerificador(cpf, multiplicador) {
+    if(multiplicador >= 12) {
+        return true
+    }
+    
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificador(cpf, multiplicador + 1   )
+    }
+
+    return false 
 }
 
 function confirmaDigito(soma) {
-    return 11 - (soma % 11);
+    return 11 - (soma % 11)
 }
