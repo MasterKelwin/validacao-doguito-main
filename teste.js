78abdedfa35f1d0a28f273e4d5d83a6c6d0fabd2
MasterKelwin <mkelwin@outlook.com>
2022-03-22 13:33:36 -0300

    testes com classes

---
+++ teste.js
@@ -0,0 +1,37 @@
+class Livro {
+    constructor(nome, editora, paginas){
+        this.nome = nome
+        this.editora = editora
+        this.paginas = paginas
+    }
+
+    anunciarTitulo(){
+        console.log(`Titulo: ${this.nome}`)
+    }
+
+    descreverTitulo(){
+        console.log(`${this.nome} é um livro da editora ${this.editora} e tem ${this.paginas} páginas`)
+    }
+}
+
+const NodeJS = new Livro("Primeiros passos com NodeJS", "Casa do Código", 195)
+
+NodeJS.anunciarTitulo()
+NodeJS.descreverTitulo()
+
+console.log(typeof Livro)
+
+class LivroColecao extends Livro {
+    constructor (nome, nomeColecao){
+        super(nome)
+        this.nomeColecao = nomeColecao
+    }
+
+    descreverColecao(){
+        console.log(`O livro ${this.nome} faz parte da coleção ${this.nomeColecao}`)
+    }
+}
+
+const logicaDeProgramacao = new LivroColecao("Logica de Programação", "Comece a programar")
+
+logicaDeProgramacao.descreverColecao()
